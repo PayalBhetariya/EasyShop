936c6f9221ef727a1f58064e59c405faa80a76e4
Payal <payalbhetariya66@gmail.com>
2024-03-02 18:42:38 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import Products from "./Products";
 
+
 function Home() {
   return (
     <div className="hero">
-      <div class="card text-bg-transperent">
-        <img
-          src="assets/banner.jpg"
-          class="card-img"
-          alt="background-image"
-          height="550px"
-        />
-        <div class="card-img-overlay d-flex flex-column">
+      <div className="card text-bg-transperent"> 
+        <img src="/assets/banner.jpg" className="card-img" alt="" height="550px"/> 
+        <div className="card-img-overlay d-flex flex-column"> 
           <div className="container">
             <h5
               style={{ color: "black" }}
-              class="card-title display-6 fw-bolder mt-4 mb-2"
+              className="card-title display-6 fw-bolder mt-4 mb-2" 
             >
               FIND YOUR STYLE WITH OUR LATEST ADDITION!
             </h5>
-            <p class="card-text lead " style={{ color: "black" }}>
+            <p className="card-text lead " style={{ color: "black" }}> 
               Whether You're Looking for Comfortable Loungewear or Stylish
               Accessories, Our Latest Additions Have Something for Every
               Occasion.
